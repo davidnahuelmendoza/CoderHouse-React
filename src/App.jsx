8cbc4bf9b1cfd606ad8546420db46a836545f32a
davidnahuelmendoza <denahuelmendoza@outlook.com>
2022-03-31 18:01:00 -0300

    Desafio 2 - NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 
 import './App.css';
+import NavBar from './components/NavBar/NavBar';
 
 function App() {
   return (
     <div>
-      <h1>Hola mundo</h1>
-    </div>
-  );
+      <h1>App</h1>
+      <NavBar />
+      
+      </div>
+  )
 }
 
 export default App;
