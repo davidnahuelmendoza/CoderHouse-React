af67da380b1b1922d8cbac5a34cae30ed50a8003
davidnahuelmendoza <denahuelmendoza@outlook.com>
2022-04-01 16:15:38 -0300

    Desafio 4 CoderHouse

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 
 import './App.css';
+import ItemCount from './components/ItemCount/ItemCount';
 import NavBar from './components/NavBar/NavBar';
 import ItemListContainer from './containers/ItemListContainer/ItemListContainer';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div>
       <NavBar />
       <ItemListContainer greeting="Bienvenido a mi app de React" />
-      </div>
+      <ItemCount initial={1} stock={10} />
+    </div>
   )
 }
 
---
+++ src/components/ItemCount/ItemCount.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+
+function ItemCount({initial, stock}) {
+    
+    const [qty, setQty] = useState(initial)
+
+    const onAdd = (qty) => {
+        alert(`Has agregado ${qty} al carrito`)
+    }
+
+    const Add = (num) => {
+        setQty(qty + num);
+    }
+
+  return (
+    <div>
+        <div>
+            <button
+            onClick={Add(-1)}
+            disabled={qty === initial ? true : null}>-</button>
+            <span>{qty}</span>
+            <button
+            onClick={Add(+1)}
+            disabled={qty === stock ? true : null}>+</button>
+        </div>
+        <div>
+            <button
+            onClick={() => onAdd(qty)}
+            disabled={stock === 0 ? true :  null}
+            >Añadir al carrito</button>
+        </div>
+    </div>
+  )
+}
+
+export default ItemCount
